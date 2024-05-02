33565570face0fbea75dcc8479674df1502c5a17
JIHYUN <84016081+YJH-jm@users.noreply.github.com>
2024-05-02 16:01:26 +0900

    Revert "Kkkk"

--- main.py
+++
@@ -1,9 +0,0 @@
-def print_hello(name_):
-    print(f"Hello, {name_}")
-    print(f"Hello, {name_}")
-    print(f"Hello, {name_}")
-    print(f"Hello, {name_}")
-
-
-if __name__ == "__main__":
-    print_hello("JH")
